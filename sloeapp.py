d2909f9dad0e2ae49d1b67ceccf0407b8ac131b5
sloe <sloe@sloecoach.com>
2015-05-09 18:43:47 +0100

    Fix config loading

--- sloeapp.py
+++ sloeapp.py
@@ -51,12 +51,12 @@ class SloeApp:
         logging.basicConfig(format="#%(levelname)s:%(filename)s::%(funcName)s#%(lineno)d at %(asctime)s\n%(message)s")
         glb_cfg = sloelib.SloeConfig.inst()
         config_spec = None
-        if len(self.params) > 0 and not self.params[0].lower().startswith("test"):
-            config_spec = sloelib.SloeConfigSpec.new_from_ini_file("config.cfg", "Loading initial config")
-        else:
+        if len(self.params) > 0 and self.params[0].lower().startswith("test"):
             logging.getLogger().setLevel(logging.INFO)
             logging.info("Loading test config file testdata/config.cfg")
             config_spec = sloelib.SloeConfigSpec.new_from_ini_file("testdata/testconfig.cfg", "Loading test config")
+        else:
+            config_spec = sloelib.SloeConfigSpec.new_from_ini_file("config.cfg", "Loading initial config")
 
         config_spec.apply_to_config(glb_cfg, "global")
 
